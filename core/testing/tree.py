f546b3c4dce5b00d7997149ad36f192b0855765c
Connor Rust <rust485@gmail.com>
2022-01-15 17:36:26 -0600

    Add comments for tree utils

--- core/testing/tree.py
+++ core/testing/tree.py
@@ -5,6 +5,12 @@ from core.model.treenode import TreeNode
 from math import ceil
 
 def build_tree(l: List[Any]) -> Optional[TreeNode]:
+	"""
+		Build a tree from a given list. With the value at index 0 being the root,
+		the index of the parent for the value at index i = ceil(i / 2) - 1.
+
+		Use None if a node should not be added for the given index.
+	"""
 	if len(l) == 0:
 		return None
 
@@ -31,6 +37,23 @@ def build_tree(l: List[Any]) -> Optional[TreeNode]:
 	return head
 
 def build_compact_tree(l: List[Any]) -> Optional[TreeNode]:
+	"""
+	Build a tree from a list - this method mimics the tree builder 
+	provided by leetcode, which allows for not specifying the children of 
+	null nodes. 
+	Example:
+		Input: [1, 2, None, 3, None, 4]
+				1
+			   2
+			  3
+			 4
+
+		Notice since the right node of the root is None, it is implied
+		that the children of that non-existent node will also be none. 
+
+		With build_tree, the input required to achieve the same output would be
+		[1, 2, None, 3, None, None, None]
+	"""
 	if len(l) == 0:
 		return None
 
@@ -58,7 +81,7 @@ def build_compact_tree(l: List[Any]) -> Optional[TreeNode]:
 				p.right = n
 			
 			mp += [n]
-			
+
 		i += 1
 
 	return mp[0]
